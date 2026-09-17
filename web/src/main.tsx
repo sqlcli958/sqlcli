3ef3aafdab23e12c83176a1703dad01df3b7e5e3
sqlcli958 <zssh993@gmail.com>
2026-09-16 21:06:44 -0700

    feat(ui): load final product polish layer

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,6 +4,7 @@ import App from './app/App';
 import './app/App.css';
 import './app/brand-theme.css';
 import './app/product-layout.css';
+import './app/product-polish.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
