5c7cba4dd976ffc984b152054c975c3cd1908dee
sqlcli958 <zssh993@gmail.com>
2026-09-16 21:05:36 -0700

    feat(ui): load product layout phase two

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './app/App';
 import './app/App.css';
 import './app/brand-theme.css';
+import './app/product-layout.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
